91e31147757c10bc7c18f7a530a23b317d8ac2fa
samuele794 <samuelebruschi794@gmail.com>
2023-12-04 11:54:09 +0100

    Add filter for not add existing repository in db

--- src/main/kotlin/com/takaotech/route/github/repository/DepositoryRepository.kt
+++ src/main/kotlin/com/takaotech/route/github/repository/DepositoryRepository.kt
@@ -1,9 +1,11 @@
 package com.takaotech.route.github.repository
 
 import com.takaotech.route.github.data.GithubDepositoryEntity
+import com.takaotech.route.github.data.GithubDepositoryTable
 import com.takaotech.route.github.data.GithubUserEntity
 import com.takaotech.route.github.model.GHRepository
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.koin.core.annotation.Factory
 
@@ -12,9 +14,23 @@ class DepositoryRepository(private val database: Database) {
 
     fun saveAllRepositoryToDB(repositoryList: List<GHRepository>) {
         transaction(database) {
-            val users = repositoryList.map {
+            val repositoryListFiltered = repositoryList.map {
+                it.id
+            }.let {
+                GithubDepositoryTable.slice(GithubDepositoryTable.id)
+                    .selectAll()
+                    .map {
+                        it[GithubDepositoryTable.id].value
+                    }
+            }.let { exclusionList ->
+                repositoryList.filterNot {
+                    exclusionList.contains(it.id)
+                }
+            }
+
+            val users = repositoryListFiltered.map {
                 it.user.let { user ->
-                    GithubUserEntity.new(user.id) {
+                    GithubUserEntity.findById(user.id) ?: GithubUserEntity.new(user.id) {
                         name = user.name
                         url = user.url
                     }
